8d32e19d631eb3099e45d91595bfe3f327b4872b
mts mts <michaelsmith101@gmail.com>
2017-08-08 23:00:53 -0700

    Checkpoint 5 Complete

---
+++ app/scripts/controllers/CookieCtrl.js
@@ -0,0 +1,18 @@
+(function() {
+  function BlocChatCookies($cookies,$uibModal) {
+    var currentUser = $cookies.get('blocChatCurrentUser');
+    console.log(currentUser);
+    if (!currentUser || currentUser === '') {
+      $uibModal.open({
+        //________Modal configuration object properties
+        templateUrl: '/templates/new_user_modal.html',
+        controller: 'UserModalCtrl',
+        controllerAs: 'usermodal'
+      })
+    }
+  }
+
+  angular
+    .module('blocChat')
+    .run(['$cookies','$uibModal', BlocChatCookies]);
+})();
